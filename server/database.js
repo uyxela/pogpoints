49aaaf9b33dba219199b6615136be80391ea66c0
Alex Yu <alexjy@yahoo.com>
2021-03-27 21:38:37 -0400

    mongo

--- server/database.js
+++ server/database.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-mongoose.connect(process.env.MONGODB_URI, { useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error: "));
